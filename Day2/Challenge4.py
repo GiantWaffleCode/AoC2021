ff0d37d3ce9e2dc7d37451825aa29b7f22ce8ade
GiantWaffleCode <waffle@giantwaffle.tv>
2021-12-02 11:06:35 -0600

    Challenge3and4

---
+++ Day2/Challenge4.py
@@ -0,0 +1,31 @@
+# -----------------------
+#   Advent of Code 2021
+#      GiantWaffle
+#   Challenge 4 of 50
+# -----------------------
+
+instructions = []
+
+with open('G:\Repo\AoC2021\Day2\Challenge3Input.txt') as f:
+   lines = f.readlines()
+   for line in lines:
+      instructions.append(line.strip())
+
+totalForward = 0;
+totalDepth = 0;
+tempAim = 0;
+
+for instruction in instructions:
+   splitList = instruction.split()
+   splitList[1] = int(splitList[1])
+
+   match splitList[0]:
+      case 'forward':
+         totalForward += splitList[1]
+         totalDepth += tempAim * splitList[1]
+      case 'up':
+         tempAim -= splitList[1]
+      case 'down':
+         tempAim += splitList[1]
+
+print(totalForward * totalDepth)
